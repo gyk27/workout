0d45596ac837752a766fad77749941dec6369246
Gururaj Kulkarni1 <gururaj.kulkarni@cognizant.com>
2018-03-05 14:59:02 +0530

    first version

---
+++ src/main/java/com/workout/controller/WorkoutTransactionController.java
@@ -0,0 +1,26 @@
+package com.workout.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.workout.entity.WorkoutTransaction;
+import com.workout.service.WorkoutTransactionService;
+
+@RestController
+public class WorkoutTransactionController {
+
+	@Autowired
+	private WorkoutTransactionService workoutTransactionService;
+
+	@RequestMapping(value = "/createWorkoutTransaction", method = RequestMethod.POST)
+	public ResponseEntity<String> createWorkoutTransaction(@RequestBody WorkoutTransaction workoutTransaction) {
+		workoutTransactionService.createWorkoutTransaction(workoutTransaction);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+
+	}
+}
